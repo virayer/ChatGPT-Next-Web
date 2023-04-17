b2d9ef495674f97116d5301af2d772167d85a470
Virayer <virayer@163.com>
2023-04-17 19:42:22 +0800

    commit

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,9 +167,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>灵叶的AI小助手</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
@@ -205,9 +205,7 @@ function _Home() {
               />
             </div>
             <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
-                <IconButton icon={<GithubIcon />} shadow />
-              </a>
+              
             </div>
           </div>
           <div>
